05fb019675ceb80c0b2ccc9d5187f40ebcebf10c
matycarolina <contrerasmarialejandra1@gmail.com>
2021-11-22 18:43:14 -0500

    refactor: :art: Dar formato al codigo

--- src/Calendar.js
+++ src/Calendar.js
@@ -1,8 +1,7 @@
 import React from "react";
-import FullCalendar from "@fullcalendar/react"; // must go before plugins
-import dayGridPlugin from "@fullcalendar/daygrid"; // a plugin!
-//import interactionPlugin from "@fullcalendar/interaction"; 
-import logo from './assets/images/logo192.png';
+import FullCalendar from "@fullcalendar/react";
+import dayGridPlugin from "@fullcalendar/daygrid"; 
+import logo from "./assets/images/logo192.png";
 
 export default class Calendar extends React.Component {
   render() {
@@ -11,7 +10,7 @@ export default class Calendar extends React.Component {
         <div>
           <b>{eventInfo.timeText}</b>
           <i>{eventInfo.event.title}</i>
-          <img src={eventInfo.event.url} style={{width:'100px'}}/>
+          <img src={eventInfo.event.url} style={{ width: "100px" }} alt={eventInfo.event.title}/>
         </div>
       );
     }
@@ -23,9 +22,12 @@ export default class Calendar extends React.Component {
         events={[
           { title: "event 1", date: "2021-11-12" },
           { title: "event 2", date: "2021-11-12", backgroundColor: "green" },
-          { title: "event 3", date: "2021-11-12", backgroundColor: "red",
-          url: logo,
-        },
+          {
+            title: "event 3",
+            date: "2021-11-12",
+            backgroundColor: "red",
+            url: logo,
+          },
           {
             title: "event 4",
             date: "2021-11-12",
@@ -36,7 +38,7 @@ export default class Calendar extends React.Component {
             title: "event 5",
             date: "2021-11-13",
             backgroundColor: "pink",
-            url: 'https://res.cloudinary.com/dekqenh3y/image/upload/v1637620922/sample.jpg',
+            url: "https://res.cloudinary.com/dekqenh3y/image/upload/v1637620922/sample.jpg",
           },
         ]}
       />
