434b083400e1469d13836c518362c400a3b78c09
matycarolina <contrerasmarialejandra1@gmail.com>
2021-12-09 16:21:06 -0600

    feat: :sparkles: Calendar by ResourceId

--- src/Calendar.js
+++ src/Calendar.js
@@ -4,6 +4,8 @@ import dayGridPlugin from "@fullcalendar/daygrid";
 import bootstrapPlugin from "@fullcalendar/bootstrap";
 import timeGridPlugin from "@fullcalendar/timegrid";
 import listPlugin from "@fullcalendar/list";
+import resourceTimeGridPlugin from "@fullcalendar/resource-timegrid";
+import resourceTimelinePlugin from "@fullcalendar/resource-timeline";
 
 import "bootstrap/dist/css/bootstrap.css";
 import "@fortawesome/fontawesome-free/css/all.css";
@@ -13,11 +15,12 @@ export default class Calendar extends React.Component {
   render() {
     function renderEventContent(eventInfo) {
       const { event } = eventInfo;
-      const { type, doctor, surgery, patient, status } = event.extendedProps;
+      const { type, doctor, surgery, patient, status, resourceId } =
+        event.extendedProps;
       const { borderColor } = event;
       const { start, end } = event;
       console.log(event);
-      console.log(start);
+      console.log(resourceId);
       return (
         <div
           className={`bg-${type} text-${type}-text`}
@@ -43,8 +46,21 @@ export default class Calendar extends React.Component {
     }
     return (
       <FullCalendar
-        plugins={[dayGridPlugin, bootstrapPlugin, timeGridPlugin, listPlugin]}
-        initialView="dayGridMonth"
+        schedulerLicenseKey="CC-Attribution-NonCommercial-NoDerivatives"
+        plugins={[
+          dayGridPlugin,
+          bootstrapPlugin,
+          timeGridPlugin,
+          listPlugin,
+          resourceTimeGridPlugin,
+          resourceTimelinePlugin,
+        ]}
+        initialView="resourceTimeGridDay"
+        resources={[
+          { id: "1", title: "Quirofano 1" },
+          { id: "2", title: "Quirofano 2" },
+          { id: "3", title: "Quirofano 3" },
+        ]}
         headerToolbar={{
           start: "prev,next title",
 
@@ -53,6 +69,7 @@ export default class Calendar extends React.Component {
         eventContent={renderEventContent}
         events={[
           {
+            id: "1",
             extendedProps: {
               type: "appointments",
               patient: "Raul Salinas",
@@ -60,11 +77,13 @@ export default class Calendar extends React.Component {
               surgery: "Apendicectomia",
               status: "Pendiente",
             },
-            start: "2021-11-06T11:00:00",
-            end: "2021-11-06T12:30:00",
+            start: "2021-12-06T11:00:00",
+            end: "2021-12-06T12:30:00",
             url: "https://res.cloudinary.com/dekqenh3y/image/upload/v1638303770/llamada-telefonica_v5ba31.png",
+            resourceIds: ["1", "2", "3"],
           },
           {
+            id: "2",
             title: "Meditation",
             extendedProps: {
               type: "appointments",
@@ -73,11 +92,12 @@ export default class Calendar extends React.Component {
               surgery: "Colecistectomia",
               status: "Pendiente",
             },
-            start: "2021-11-10T10:00:00",
-            end: "2021-11-10T11:00:00",
+            start: "2021-12-10T10:00:00",
+            end: "2021-12-10T11:00:00",
             url: "https://res.cloudinary.com/dekqenh3y/image/upload/v1638303770/llamada-telefonica_v5ba31.png",
           },
           {
+            id:"3",
             title: "Doctor's Appointment",
             extendedProps: {
               type: "appointments",
@@ -86,11 +106,13 @@ export default class Calendar extends React.Component {
               surgery: "Sutura fractura expuesta",
               status: "Pendiente",
             },
-            start: "2021-11-12T06:00:00",
-            end: "2021-11-12T08:00:00",
+            start: "2021-12-12T06:00:00",
+            end: "2021-12-12T08:00:00",
             url: "https://res.cloudinary.com/dekqenh3y/image/upload/v1638303770/llamada-telefonica_v5ba31.png",
+            resourceIds: ["1", "2", "3"],
           },
           {
+            id:"4",
             title: "Doctor's Appointment",
             extendedProps: {
               type: "appointments",
@@ -99,9 +121,10 @@ export default class Calendar extends React.Component {
               surgery: "Sutura fractura expuesta",
               status: "Completo",
             },
-            start: "2021-11-12T08:00:00",
-            end: "2021-11-12T10:00:00",
+            start: "2021-12-12T08:00:00",
+            end: "2021-12-12T10:00:00",
             url: "https://res.cloudinary.com/dekqenh3y/image/upload/v1638303770/llamada-telefonica_v5ba31.png",
+            resourceIds: ["1", "2", "3"],
           },
         ]}
       />
